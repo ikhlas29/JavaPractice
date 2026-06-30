1acb407fb8a43eef33e968da3bd2530419128f0a
Ikhlas Al Khusaibi <ekhlasalkhusaibi@gmail.com>
2026-06-30 08:55:33 +0400

    Add practice mortgage project

---
+++ PracticeArea/src/ProjectPractice.java
@@ -0,0 +1,28 @@
+// Project --> Mortgage Calculator
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class ProjectPractice {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / 100 / 12;
+
+        System.out.print("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * 12;
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
+    }
+}
